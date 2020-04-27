0d47d18dcd0274e8accc11bee60f50922d91a45a
Abhishek Trivedi <abhishektrivedi1697@gmail.com>
2020-04-27 22:20:07 +0530

    Update bbox_extractor.py

--- Code/bbox_extractor.py
+++ Code/bbox_extractor.py
@@ -1,21 +1,19 @@
-
-# coding: utf-8
-
-# In[23]:
-
-
 import cv2
 import numpy as np
 import os
 import sys
 
+class BoundingBox(object):
+	def __init__(self):
+		self.bboxs = []
 
-# In[50]:
+	def run_bbox(self):
+		for i in sorted(os.listdir('./test_images/')):
+			if '.jpg' in i:
+				print('Finding faces in image',i)
+				os.system("python yoloface/yoloface.py --image test_images/"+i+" --output-dir output_bbox/")
+		print('All bounding boxes extracted successfully')
 
-
-for i in sorted(os.listdir('./test_images/')):
-    if '.jpg' in i:
-        print('Finding faces in image',i)
-        os.system("python yoloface/yoloface.py --image test_images/"+i+" --output-dir output_bbox/")
-print('All bounding boxes extracted successfully')
-
+if __name__ == '__main__':
+    bbox = BoundingBox()
+    bbox.run_bbox()
