ae54806b5fbeb1dbfd4b63eda61624734882de44
Maanik Arora <30871591+Hooligan10@users.noreply.github.com>
2020-04-27 19:43:31 +0530

    Update utils.py

--- Code/yoloface/utils.py
+++ Code/yoloface/utils.py
@@ -1,18 +1,3 @@
-# *******************************************************************
-#
-# Author : Thanh Nguyen, 2018
-# Email  : sthanhng@gmail.com
-# Github : https://github.com/sthanhng
-#
-# BAP, AI Team
-# Face detection using the YOLOv3 algorithm
-#
-# Description : utils.py
-# This file contains the code of the parameters and help functions
-#
-# *******************************************************************
-
-
 import datetime
 import numpy as np
 import cv2
